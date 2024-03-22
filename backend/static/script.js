ddf0403acb5d6116229b0f410d2b3037984d3b9f
elizabeth-tang <eliza.n.tang@gmail.com>
2024-03-22 04:30:25 -0400

    show matching results from face products

--- backend/static/script.js
+++ backend/static/script.js
@@ -6,39 +6,30 @@ const disSearchBox = document.querySelector("#dis-search-box");
 const disAutoBox = document.querySelector("#dis-auto-box");
 const disInputBox = document.querySelector("#dis-search-text");
 
-function answerBoxTemplate(title, titleDesc, rating) {
+function answerBoxTemplate(product,category,link,price){
   return `<div class=''>
-      <h3 class='episode-title'>${title}</h3>
-      <p class='episode-desc'>${titleDesc}</p>
-      <p class='episode-rating'>IMDB Rating: ${rating}</p>
-  </div>`;
+      <h3 class='product-name'>${product}</h3>
+      <p class='product-category'>${category}</p>
+      <p class='product-price'>${price}</p>
+      <p class='product-link'>${link}</p>
+  </div>`
 }
 
 function sendFocus() {
   productSearchBox.focus();
 }
 
-function filterText() {
-  document.getElementById("answer-box").innerHTML = "";
-  console.log(document.getElementById("filter-text-val").value);
-  fetch(
-    "/episodes?" +
-      new URLSearchParams({
-        title: document.getElementById("filter-text-val").value,
-      }).toString()
-  )
-    .then((response) => response.json())
-    .then((data) =>
-      data.forEach((row) => {
-        let tempDiv = document.createElement("div");
-        tempDiv.innerHTML = answerBoxTemplate(
-          row.title,
-          row.descr,
-          row.imdb_rating
-        );
-        document.getElementById("answer-box").appendChild(tempDiv);
-      })
-    );
+function filterText(){
+  document.getElementById("answer-box").innerHTML = ""
+  console.log(document.getElementById("filter-text-val").value)
+  fetch("/episodes?" + new URLSearchParams({ title: document.getElementById("filter-text-val").value }).toString())
+  .then((response) => response.json())
+  .then((data) => data.forEach(row => {
+      
+      let tempDiv = document.createElement("div")
+      tempDiv.innerHTML = answerBoxTemplate(row.product,row.category,row.link,row.price)
+      document.getElementById("answer-box").appendChild(tempDiv)
+  }));
 }
 
 function setProductClickable(list) {
@@ -53,11 +44,24 @@ function selectProduct(element) {
   productSearchBox.classList.remove("active");
 }
 
-function showProducts() {
+async function matchProducts() {
+  var match = ''
+  await fetch("/search?" + new URLSearchParams({ title: document.getElementById("prod-search-text").value }).toString())
+  .then((response) => response.json())
+  .then((data) => data.forEach(row => {
+    match += `<li>${row.product}</li>`
+    console.log(match)
+  }));
+  console.log('outside')
+  console.log(match)
+  console.log(document.getElementById("prod-search-text").value)
+  return match
+}
+
+async function showProducts() {
   if (productInputBox.value != "") {
     productSearchBox.classList.add("active");
-    productAutoBox.innerHTML =
-      "<li>product 1</li><li>product 2</li><li>product 3</li>";
+    productAutoBox.innerHTML = await matchProducts();
     allList = productAutoBox.querySelectorAll("li");
     setProductClickable(allList);
   } else {
